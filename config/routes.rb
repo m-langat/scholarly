e4e9536e148c2475a52b732e633478cd198bd8af
Michael Langat <langatkipmichael@gmail.com>
2022-10-28 19:51:01 +0300

    removed subthemes and themes routes frm route file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :conference_themes
   resources :comments
   resources :papers
-  resources :subthemes
-  resources :themes
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
